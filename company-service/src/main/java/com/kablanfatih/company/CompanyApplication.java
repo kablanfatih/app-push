be09473cf0170fb7a105d12937641601a2a5f587
Fatih Kablan <kablanfatih34@gmail.com>
2021-02-07 16:51:04 +0300

    content service created

--- company-service/src/main/java/com/kablanfatih/company/CompanyApplication.java
+++ company-service/src/main/java/com/kablanfatih/company/CompanyApplication.java
@@ -6,11 +6,13 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableJpaAuditing(auditorAwareRef = "auditorAware")
 public class CompanyApplication {
 
